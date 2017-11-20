e5b357b969f04f5747d4126eb4e264b2e3d99a1c
王强 <heartinfei@gmail.com>
2017-11-20 16:15:40 +0800

    add 事件过滤

--- app/src/main/java/com/dianxiaohuo/flowabletest/DebounceActivity.java
+++ app/src/main/java/com/dianxiaohuo/flowabletest/DebounceActivity.java
@@ -8,24 +8,35 @@ import android.view.View;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+
 /**
  * 简介：
- * @author 王强
+ *
+ * @author 王强 on 2017/11/20 249346528@qq.com
  */
 public class DebounceActivity extends AppCompatActivity implements View.OnClickListener {
+    ObservableEmitter<Integer> emitter;
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_debounce);
-
+        Observable.create((ObservableEmitter<Integer> e) -> emitter = e)
+                .debounce(1, TimeUnit.SECONDS)
+                .subscribe((i) -> Log.i("WTEST", "Exec:" + i));
     }
 
     @Override
     public void onClick(View v) {
         Log.i("WTEST", "click");
-        Observable.just(v.getId())
-                .debounce(1, TimeUnit.SECONDS)
-                .subscribe((i) -> Log.i("WTEST", "Exec:" + i));
+        emitter.onNext(v.getId());
+
     }
 }
